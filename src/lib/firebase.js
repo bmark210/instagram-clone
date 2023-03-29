b4e1c2da761ea2e3f9ce801a8b2fe77f9f3e4f84
Mark <105296060+bmark210@users.noreply.github.com>
2023-03-30 00:53:50 +0300

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,7 @@ import { getAuth } from "firebase/auth";
 // import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: 'AIzaSyCnLWuFAUA7ahql5F64cPmnojlNjV7xWeM',
-  authDomain: 'instagram-clone-e904f.firebaseapp.com',
-  projectId: 'instagram-clone-e904f',
-  storageBucket: 'instagram-clone-e904f.appspot.com',
-  messagingSenderId: '522313280912',
-  appId: '1:522313280912:web:c44e1c273b73eadd251715',
+  // here your config
 };
 const firebase = initializeApp(config);
 const firestore = getFirestore();
